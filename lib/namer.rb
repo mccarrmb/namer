e5219b209b51ab98aa6ac02485e68d7847e37928
Whitey McCarragher <matt.mccarragher@online-rewards.com>
2018-07-13 16:29:32 -0400

    added words

--- lib/namer.rb
+++ lib/namer.rb
@@ -1,8 +1,8 @@
 require "namer/version"
 
 module Namer
-  @@verbs = ["farty"]
-  @@nouns = ["baby"]
+  @@verbs = ["running", "burning", "eating", "walking", "fighting", "flying", "stomping", "digesting"]
+  @@nouns = ["baby", "car", "book", "piano", "block", "ninja", "warrior", "marine", "airman", "toaster"]
   def self.do
     @@verbs[rand*@@verbs.length] + "-" + @@nouns[rand*@@nouns.length]
   end
