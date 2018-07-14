4b6c6867367f45a791e53766b40fa09863be0d15
Whitey <mccarrmb@users.noreply.github.com>
2018-07-14 19:44:42 -0400

    added ruby code coverage thingy

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,9 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "namer"
 
 require "minitest/autorun"
+
+require 'simplecov'
+SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
+SimpleCov.start do
+    add_filter ['.bundle', 'test']
+end
