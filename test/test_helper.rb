8c0b245ad279d84f90f6367e6b41dee5a951e323
Whitey <mccarrmb@users.noreply.github.com>
2018-07-14 20:40:44 -0400

    modified travis and added gitattr cuz windows

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,10 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "namer"
-
-require "minitest/autorun"
-
 require 'simplecov'
 SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
 SimpleCov.start do
     add_filter ['.bundle', 'test']
 end
+
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "namer"
+
+require "minitest/autorun"
