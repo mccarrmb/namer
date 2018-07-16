c5812781d3a6aa4b05c58743863c810e0e92817a
Whitey McCarragher <matt.mccarragher@online-rewards.com>
2018-07-16 08:40:54 -0400

    added de verbs/nouns

--- lib/namer.rb
+++ lib/namer.rb
@@ -1,10 +1,19 @@
 require "namer/version"
+require 'locale'
 
 module Namer
   @@verbs = ["running", "burning", "eating", "walking", "fighting", "flying", "stomping", "digesting"]
   @@nouns = ["baby", "car", "book", "piano", "block", "ninja", "warrior", "marine", "airman", "toaster"]
-  def self.do
+  @@verbs_de = ["laufen","brennen","essen","gehen","kampfen","fliegen","stampfen","verdauen"]
+  @@nouns_de = ["baby","auto","buch","klavier","block","ninja","krieger","marine","flieger","toaster"]
+  
+  def self.do(language = "en")
+    if (Locale.current.language.eql?("de") || languag.eql?("de"))
+      @@nouns_de[rand*@@nouns_de.length] + "-" + @@verbs_de[rand*@@verbs_de.length]
+    end
     @@verbs[rand*@@verbs.length] + "-" + @@nouns[rand*@@nouns.length]
   end
+
+
 
 end
