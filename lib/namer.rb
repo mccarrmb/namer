0d8d24483864f8c2cf00bebc12e96701e23f25ac
Whitey McCarragher <matt.mccarragher@online-rewards.com>
2018-07-16 08:43:58 -0400

    fixed typo

--- lib/namer.rb
+++ lib/namer.rb
@@ -8,7 +8,7 @@ module Namer
   @@nouns_de = ["baby","auto","buch","klavier","block","ninja","krieger","marine","flieger","toaster"]
   
   def self.do(language = "en")
-    if (Locale.current.language.eql?("de") || languag.eql?("de"))
+    if (Locale.current.language.eql?("de") || language.eql?("de"))
       @@nouns_de[rand*@@nouns_de.length] + "-" + @@verbs_de[rand*@@verbs_de.length]
     end
     @@verbs[rand*@@verbs.length] + "-" + @@nouns[rand*@@nouns.length]
