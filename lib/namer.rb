2ce18d9736b6c101bee239687bcfbb71016e46f3
Whitey McCarragher <matt.mccarragher@online-rewards.com>
2018-07-19 11:46:27 -0400

    rubocop fixes

--- lib/namer.rb
+++ lib/namer.rb
@@ -1,10 +1,9 @@
-require "namer/version"
+require 'namer/version'
 
 module Namer
-  @@verbs = ["running", "burning", "eating", "walking", "fighting", "flying", "stomping", "digesting"]
-  @@nouns = ["baby", "car", "book", "piano", "block", "ninja", "warrior", "marine", "airman", "toaster"]
   def self.do
-    @@verbs[rand*@@verbs.length] + "-" + @@nouns[rand*@@nouns.length]
+    verbs = %w[running burning eating walking fighting flying stomping digesting]
+    nouns = %w[baby car book piano block ninja warrior marine airman toaster]
+    verbs[rand * verbs.length] + '-' + nouns[rand * nouns.length]
   end
-
 end
--- test/test_namer.rb
+++ test/test_namer.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class NamerTest < Minitest::Test
   def test_that_it_has_a_version_number
@@ -6,14 +6,14 @@ class NamerTest < Minitest::Test
   end
 
   def test_does_it_not_blow_up
-    assert(::Namer::do)
+    assert(::Namer.do)
   end
 
   def test_does_it_return_a_string
-    assert((::Namer::do).is_a?(String))
+    assert(::Namer.do.is_a?(String))
   end
 
   def test_does_it_return_different_strings_every_call
-    assert(!::Namer::do.eql?(::Namer::do))
+    assert(!::Namer.do.eql?(::Namer.do))
   end
 end
