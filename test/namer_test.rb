ddf3093a1ea7c79e103c499c5c34da8df284b5f3
Whitey McCarragher <matt.mccarragher@online-rewards.com>
2018-08-01 14:09:55 -0400

    added parallelization

--- test/namer_test.rb
+++ test/namer_test.rb
@@ -1,6 +1,8 @@
 require "test_helper"
 
 class NamerTest < Minitest::Test
+  parallelize_me!
+
   def test_that_it_has_a_version_number
     refute_nil ::Namer::VERSION
   end
@@ -13,7 +15,7 @@ class NamerTest < Minitest::Test
     assert((::Namer::do).is_a?(String))
   end
 
-  def test_does_it_return_different_strings_every_call
-    assert(!::Namer::do.eql?(::Namer::do))
+  def test_does_it_return_any_whitespace_characters
+    assert((::Namer::do =~ /\s/).nil?)
   end
 end
