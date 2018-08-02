18d09f10d872cb1cc9d497cfe89cc7e1d4ea8071
Whitey McCarragher <matt.mccarragher@online-rewards.com>
2018-08-02 08:22:08 -0400

    presentation startpoint

--- test/namer_test.rb
+++ test/namer_test.rb
@@ -6,14 +6,14 @@ class NamerTest < Minitest::Test
   end
 
   def test_does_it_not_blow_up
-    assert(::Namer::do('de'))
+    assert(::Namer::do)
   end
 
   def test_does_it_return_a_string
     assert((::Namer::do).is_a?(String))
   end
 
-  def test_does_it_return_different_strings_every_call
-    assert(!::Namer::do.eql?(::Namer::do))
+  def test_does_it_return_any_whitespace_characters
+    assert((::Namer::do =~ /\s/).nil?)
   end
 end
